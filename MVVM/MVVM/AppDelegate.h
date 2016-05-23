483b72e5e991b2935db217e8014aa70fd1adbabf
lauyung <ljy123liu@gmail.com>
2016-05-23 13:35:16 +0800

    push to github

---
+++ MVVM/MVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM
+//
+//  Created by LIUYONG on 16/5/23.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVM/MVVM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MVVM
+//
+//  Created by LIUYONG on 16/5/23.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
